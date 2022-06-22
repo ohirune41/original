1340cecaa48ec7ee263e991223aa08051277500b
ohirune41 <jiben8628@gmail.com>
2022-06-22 20:04:03 +0900

    詳細画面に映画情報の表示と配置調整

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,12 @@
 class MoviesController < ApplicationController
   require 'themoviedb-api'
   Tmdb::Api.key('533590b90e868ee5686d05e56edc02c1')
-  Tmdb::Api.language('ja') # こちらで映画情報の表示の際の言語設定を日本語にできます
+  #Tmdb::Api.language("ja") # 映画情報の言語設定を日本語にできる
 
   def index
   end
 
   def show
+    
   end
 end
