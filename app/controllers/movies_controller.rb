755da1cd6de56686245cb4929ef09c1db8570258
ohirune41 <jiben8628@gmail.com>
2022-06-22 20:05:49 +0900

    コードの整理

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,12 +1,11 @@
 class MoviesController < ApplicationController
   require 'themoviedb-api'
   Tmdb::Api.key('533590b90e868ee5686d05e56edc02c1')
-  #Tmdb::Api.language("ja") # 映画情報の言語設定を日本語にできる
+  # Tmdb::Api.language("ja") # 映画情報の言語設定を日本語にできる
 
   def index
   end
 
   def show
-    
   end
 end
